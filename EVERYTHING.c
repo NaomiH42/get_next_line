dcc6339d29d0fd6a825008a37a62b1b725ade9ed
Evelyn Hasalu <ehasalu@c2r6s3.42prague.com>
2023-02-04 16:10:37 +0100

    pain

--- EVERYTHING.c
+++ EVERYTHING.c
@@ -1,16 +1,90 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ehasalu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/24 22:49:08 by ehasalu           #+#    #+#             */
-/*   Updated: 2023/02/04 13:51:04 by ehasalu          ###   ########.fr       */
+/*   Created: 2023/01/25 14:43:42 by ehasalu           #+#    #+#             */
+/*   Updated: 2023/02/01 21:49:21 by ehasalu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
 #include "get_next_line.h"
+#include <stdio.h>
+#include <limits.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*temp;
+	size_t	test;
+
+	if (nmemb == 0 || size == 0)
+	{
+		temp = malloc(0);
+		return (temp);
+	}
+	test = nmemb * size;
+	if (test / nmemb != size)
+		return (NULL);
+	temp = malloc(size * nmemb);
+	if (!temp)
+		return (NULL);
+	ft_memset(temp, 0, size * nmemb);
+	return (temp);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (i);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	char	*res;
+
+	i = 0;
+	res = (char *)ft_calloc(sizeof(char), (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!res)
+		return (NULL);
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (*s2)
+	{
+		res[i] = *s2;
+		i++;
+		s2++;
+	}
+	res[i] = '\0';
+	free (s1);
+	return (res);
+}
 
 char	*ft_strdup(char *s)
 {
@@ -33,6 +107,33 @@ char	*ft_strdup(char *s)
 	return (dest);
 }
 
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)s)[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
+
+char	*ft_strrchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+//	i = ft_strlen(s);
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i++;
+	}
+	return (NULL);
+}
 void trim(char **line)
 {
 	size_t	l;
@@ -82,6 +183,7 @@ char	*cut_extra(char *line)
 	return (ret);
 }
 
+#include <stdio.h>
 char	*read_fd(char **buffer, int fd)
 {
 	int	bytes;
@@ -129,6 +231,39 @@ char	*get_next_line(int fd)
 		buf[fd] = (char *)ft_calloc(sizeof(char), (BUFFER_SIZE + 1));	
 	}
 	line = read_fd(&buf[fd], fd);
-	trim(&buf[fd]);
+	trim(&buf[fd]);//ft_strrchr(buf[fd], '\n');
 	return (line);
+}
+
+int	main(void)
+{
+	int	fd1;
+	char	*l;
+	
+	
+	fd1 = open("file.xt", O_RDWR);
+//	get_next_line(fd1);
+//	get_next_line(fd1);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	close(fd1);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ehasalu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 14:30:14 by ehasalu           #+#    #+#             */
-/*   Updated: 2023/02/01 21:40:21 by ehasalu          ###   ########.fr       */
+/*   Updated: 2023/02/04 15:05:13 by ehasalu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,36 @@
 int	main(void)
 {
 	int	fd1;
+	char	*l;
 	
-	fd1 = open("text.txt", O_RDWR);
-//	get_next_line(fd1);
-//	get_next_line(fd1);
-	printf("%s", get_next_line(fd1));
-	printf("%s", get_next_line(fd1));
-	printf("%s", get_next_line(fd1));
-	printf("%s", get_next_line(fd1));
+	
+	fd1 = open("files/alternate_line_nl_with_nl", O_RDWR);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
+	l = get_next_line(fd1);
+	printf("%s", l);
+	free(l);
 	close(fd1);
 }
